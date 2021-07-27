cd20e2640af2f829544b1159459e9c4de847c7e2
GorokhovNS <gorokhov.ns@yandex.ru>
2021-07-27 23:07:05 +0300

    background added

--- core/src/ru/spaceshooter/StarGame.java
+++ core/src/ru/spaceshooter/StarGame.java
@@ -9,27 +9,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion region;
 
-	private int x = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 25, 25, 100, 50);
+		img = new Texture("planet-earth-in-space.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		x++;
-		ScreenUtils.clear(0.4f, 0.24f, 0.51f, 1);
+		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, x, 0);
-		batch.setColor(0.56f, 0.32f, 0.67f, 0.8f);
-		batch.draw(img, 300, 300, 100, 100);
-		batch.setColor(1f, 1f, 1f, 0.8f);
-		batch.draw(region, 230, 0 , 100, 50);
+		batch.draw(img, 0, 0, 680, 480);
 		batch.end();
 	}
 	
